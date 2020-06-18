c8ea0fd91bb5ab1bb0315f593fcb75303354a4e4
trang.tran@isobar.com <trang.tran@isobar.com>
2020-06-18 14:53:58 +0700

    Updates

---
+++ precache-manifest.cc4b19bfd51a4d02280061c2a44cf452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4752740d67cc70c645b27f6432667948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965dcedafb10bc47de2f",
+    "url": "/static/css/main.4c927cc4.chunk.css"
+  },
+  {
+    "revision": "a17538b9bb5d8743d9f6",
+    "url": "/static/js/2.1d38fc46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d38fc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965dcedafb10bc47de2f",
+    "url": "/static/js/main.482180ec.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  }
+]);
